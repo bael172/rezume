5a9e01b1a164c4737ad14a8a4dead71e42accf91
Абдималиков Байэл Эрнистович <b.abdimalikov@mkit.local>
2024-10-22 12:29:47 +0300

    project launch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import logo from './logo.svg';
+import photo from './no_name.jpg'
 import './App.css';
+const arr_headers=["Опыт работы","Образование","Курсы","О себе"]
+const photo_css ={
+  marginTop:"2%",
+  marginLeft:"2%",
+  width:"5em",
+}
+const header_css={
+  fontFamily:"Arial, Verdana, sans-serif",
+  fontSize:"2em",
+  fontWeight:"700"
+}
+
+function Photo(){
+  return( <img src={photo} style={photo_css}></img>)
+}
+function Header(){
+  return(
+      arr_headers.map((item,index) => {
+      return <h1 style={header_css}>{item}</h1>
+      })
+  )
+}
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Photo></Photo>
+    <Header></Header>
+    </>
+  );
+}
+/*
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +49,6 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
+*/ 
 
 export default App;
