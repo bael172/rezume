77c8031fdc69a5d87d4a277d7f6113634db9f61d
bael172 <bael171104@gmail.com>
2024-10-25 16:41:04 +0300

    Добавлены разделы Образование и Работа

--- src/App.js
+++ src/App.js
@@ -11,52 +11,135 @@ import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 
 import logo from './logo.svg';
-import photo from './no_name.jpg'
+import photo from './oleg_mongol.jpg'
+import mortarboard from './mortarboard.png'
+import {ReactComponent as Briefcase} from "./briefcase.svg"
 import './App.css';
 const photo_css ={
-  marginTop:"2%",
-  marginLeft:"2%",
-  width:"5em",
+  display:"block",
+  minWidth:"1em",
+  maxWidth:"10em"
 }
 const header_css={
   fontFamily:"Arial, Verdana, sans-serif",
-  fontSize:"2em",
-  fontWeight:"700"
+  fontWeight:"100",
+  margin:0
+}
+
+const nickname={
+  fontFamily:"Arial, Verdana, sans-serif",
+  fontSize:"50px"
+}
+
+const grey = {
+  color:'grey', 
+  fontSize:'1em'
+}
+
+const header_blue = {
+  color:"blue", 
+  fontSize:'1.2em', 
+  verticalAlign:'top'
+}
+
+const icon = {
+  display:"inline-block",
+  height:"20px",
+  width: "auto",
+  marginRight:"20px", 
+  marginLeft:"20px"
+}
+
+const hr_wd={
+  textAlign:"left",
+  width:"40%",
+  margin: 0
 }
 
 function Photo(){
-  return( <img src={photo} style={photo_css}></img>)
+  return( 
+    <div className="flex">
+      <img src={photo} style={photo_css}></img>
+      <dir style={nickname}>Олег Монгол Артурович</dir>
+    </div>
+  )
 }
-function Header(){
+
+function Header1(){
     return( 
-      <h1 style={header_css}>"Опыт работы"</h1>
+      <>
+        <div className="flex">
+        <Briefcase style={icon}></Briefcase>
+          <h1 style={header_css}>Опыт работы</h1>
+        </div>
+        <hr style={hr_wd}></hr>
+      </>
     )
+}
+function Header2(){
+  return(
+    <>
+      <div className="flex">
+        <img src={mortarboard} style={icon}></img>
+        <h1 style={header_css}>Образование</h1>
+      </div>
+      <hr style={hr_wd}></hr>
+    </>
+  )
 }
 let freelancer_achievs = ["Освоил 3-D виртуализацию, расширил линейку предоставляемых услуг"]
 let mesto_raboty = ["Фрилансер","Веб-агентство Spectr Design"]
 let freelancer_arr = ["Создание сайтов \"с нуля \"","Разработка структуры сайта","Разработка дизайн-макетов","Переговоры с заказчиками","Составление брифа"]
 let spectr_arr = ["Дизайн и редизайн сайтов","Дизайн макетов","Логотипы, фирменный стиль","Создание ТЗ","Координирование группы веб-разработчиков","Частичный документооборот"]
-const obj = {
-  "2016-н.в.": [mesto_raboty[0],<li>{freelancer_arr}</li>,<bold>Достижения (за 1-год)</bold>, <li>{freelancer_achievs}</li>],
-  "2010-2016": [mesto_raboty[1]+"Веб-дизайнер",<bold>Обязанности:</bold>,<li>{spectr_arr}</li>]
-}
-function Content(){
-      for(let index in obj){
-        console.log(index)
-        return(
-          <div style={{color:'grey', fontSize:'0.8em'}}>{index}</div>
-        )
+let period = ["2016-н.в.","2010-2016"]
+let freelancer_arr_li = freelancer_arr.map(item => <li>{item}</li>)
+let freelancer_achievs_li = freelancer_achievs.map(item => <li>{item}</li>)
 
-      }
+let spectr_arr_li = spectr_arr.map(item => <li>{item}</li>)
+const obj = new Map
+obj.set(period[0],[mesto_raboty[0],freelancer_arr, freelancer_achievs ])
+obj.set(period[1],[mesto_raboty[1],"Веб дизайнер",spectr_arr])
+function Content1(){
+  return(
+    <article className="container">
+      <div style={grey} >{period[0]}</div>
+      <div> 
+        <div style={header_blue}>{mesto_raboty[0]}</div>
+        <ul style={{margin:0}}>{freelancer_arr_li}</ul>
+        <b>Достижения (за 1-год)</b>
+        <ul style={{margin:0}}>{freelancer_achievs_li}</ul>
+      </div>
+      <div style={grey}>{period[1]}</div>
+        <div>
+          <div style={{display:"inline-block"}}>  
+            <div style={header_blue}>{mesto_raboty[1]}</div> 
+            <div>Веб дизайнер</div>
+          </div>
+          <b style={{display:"block"}}>Обязанности</b>
+          <ul>{spectr_arr_li}</ul>
+        </div>
+
+    </article>
+  )
+}
+function Content2(){
+  return (
+    <article className="container">
+    <div>2025</div>
+    <div style={header_blue}>МКИТ</div>
+  </article>
+  )
 }
 
 function App() {
   return (
-    <>
+    <div className="main">
     <Photo></Photo>
-    <Header></Header>
-    <Content></Content>
-    </>
+    <Header1></Header1>
+    <Content1></Content1>
+    <Header2></Header2>
+    <Content2></Content2>
+    </div>
   );
 }
 /*
