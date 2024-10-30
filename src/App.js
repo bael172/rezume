128b38bbde79fba57c8b2e625084c34188f744d6
bael172 <bael171104@gmail.com>
2024-10-30 16:33:17 +0300

    done

--- src/App.js
+++ src/App.js
@@ -10,36 +10,62 @@ import Paper from "@mui/material/Paper"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 
+import {ReactComponent as Briefcase} from "./briefcase.svg"
+import './App.css';
+
 import logo from './logo.svg';
 import photo from './oleg_mongol.jpg'
 import mortarboard from './mortarboard.png'
-import {ReactComponent as Briefcase} from "./briefcase.svg"
-import './App.css';
+import account from './account.png'
+import location from './location.png'
+import info from './info.png'
+import contact_mail from './contact-mail.png'
+import phone from './phone.png'
+import mail from './arroba.png'
+import gmail from './gmail.png'
+import whatsapp from "./whatsapp_1.png"
+import telegram from "./telegram.png"
+import translate from './translate.png'
+import pencil from './pencil.png'
+
 const photo_css ={
   display:"block",
-  minWidth:"1em",
-  maxWidth:"10em"
+  width:'auto',
+  height:'auto',
+  maxWidth:"10em",
+  maxHeight:"10em",
+  margin: "0 0 30px",
+  border: "2px black solid"
 }
-const header_css={
+const header_1={
   fontFamily:"Arial, Verdana, sans-serif",
   fontWeight:"100",
-  margin:0
+  margin:"0 0 5px 0"
 }
-
-const nickname={
-  fontFamily:"Arial, Verdana, sans-serif",
-  fontSize:"50px"
-}
-
-const grey = {
-  color:'grey', 
-  fontSize:'1em'
-}
-
-const header_blue = {
+const header_2 = {
   color:"blue", 
   fontSize:'1.2em', 
   verticalAlign:'top'
+}
+const text_1 = {
+  color:"black",
+  fontSize:"1em"
+}
+const text_2 = {
+  color:"black",
+  fontSize:"0.8em"
+}
+const nickname={
+  fontFamily:"Arial, Verdana, sans-serif",
+  fontSize:"50px",
+  //verticalAlign:"top",
+  //padding:"0",
+  margin:"0",
+  height:"auto"
+}
+const grey = {
+  color:'grey', 
+  fontSize:'1em'
 }
 
 const icon = {
@@ -49,44 +75,55 @@ const icon = {
   marginRight:"20px", 
   marginLeft:"20px"
 }
-
 const hr_wd={
-  textAlign:"left",
-  width:"40%",
-  margin: 0
+  textAlign:"right",
+  minWidth:"250px",
+  maxWidth:"25%",
+  margin:"0 0 20px 0",
+  //marginDown:"20px"
+}
+const circle_css={
+
 }
 
 function Photo(){
   return( 
+    <>
     <div className="flex">
-      <img src={photo} style={photo_css}></img>
-      <dir style={nickname}>Олег Монгол Артурович</dir>
+      <img src={photo} style={photo_css} height="80" width="120"></img>
+      <dir style={{display:"block", paddingLeft:"0", margin:"0"}}>
+        <dir style={nickname}>Олег Монгол Артурович</dir>
+        <dir style={{textAlign:"left", fontWeight:"600"}}>WEB-разработчик</dir>
+      </dir>
     </div>
+    <div style={{display:"inline-block", margin:"0 0 20px"}}>
+      <img src={location} style={{width:"20px"}}></img>
+      <div style={{display:"inline", margin:"0 5em 0 0"}}> г.Ош, Кыргызская республика</div>
+      <img src={info} style={{width:"15px"}}></img>
+      <div style={{display:"inline"}}> Возраст: 20 лет</div>
+    </div>
+    </>
   )
 }
 
-function Header1(){
-    return( 
-      <>
-        <div className="flex">
-        <Briefcase style={icon}></Briefcase>
-          <h1 style={header_css}>Опыт работы</h1>
-        </div>
-        <hr style={hr_wd}></hr>
-      </>
-    )
-}
-function Header2(){
+function Header_1({prop}){
+    let header
+    let img
+    if(prop.name == "Опыт работы") header = <Briefcase style={icon}></Briefcase>
+    if(prop.name != "Опыт работы") img = <img src={prop.icon_src} style={icon}></img>
+    
   return(
-    <>
+  <>
       <div className="flex">
-        <img src={mortarboard} style={icon}></img>
-        <h1 style={header_css}>Образование</h1>
+        {header}
+        {img}
+        <h1 style={header_1}>{prop.name}</h1>
       </div>
       <hr style={hr_wd}></hr>
     </>
   )
 }
+
 let freelancer_achievs = ["Освоил 3-D виртуализацию, расширил линейку предоставляемых услуг"]
 let mesto_raboty = ["Фрилансер","Веб-агентство Spectr Design"]
 let freelancer_arr = ["Создание сайтов \"с нуля \"","Разработка структуры сайта","Разработка дизайн-макетов","Переговоры с заказчиками","Составление брифа"]
@@ -99,12 +136,13 @@ let spectr_arr_li = spectr_arr.map(item => <li>{item}</li>)
 const obj = new Map
 obj.set(period[0],[mesto_raboty[0],freelancer_arr, freelancer_achievs ])
 obj.set(period[1],[mesto_raboty[1],"Веб дизайнер",spectr_arr])
+
 function Content1(){
   return(
     <article className="container">
       <div style={grey} >{period[0]}</div>
       <div> 
-        <div style={header_blue}>{mesto_raboty[0]}</div>
+        <div style={header_2}>{mesto_raboty[0]}</div>
         <ul style={{margin:0}}>{freelancer_arr_li}</ul>
         <b>Достижения (за 1-год)</b>
         <ul style={{margin:0}}>{freelancer_achievs_li}</ul>
@@ -112,8 +150,7 @@ function Content1(){
       <div style={grey}>{period[1]}</div>
         <div>
           <div style={{display:"inline-block"}}>  
-            <div style={header_blue}>{mesto_raboty[1]}</div> 
-            <div>Веб дизайнер</div>
+            <div style={header_2}>{mesto_raboty[1]}<div style={{color:"black"}}>Веб дизайнер</div></div> 
           </div>
           <b style={{display:"block"}}>Обязанности</b>
           <ul>{spectr_arr_li}</ul>
@@ -125,20 +162,134 @@ function Content1(){
 function Content2(){
   return (
     <article className="container">
-    <div>2025</div>
-    <div style={header_blue}>МКИТ</div>
+    <div style={grey}>2021-2025</div>
+    <div style={header_2}>МКИТ Колледж информационных технологий
+      <div style={text_1}>
+        09.02.07 Информационные системы и программирование
+        <ul style={text_2}>
+          <li>Умение создавать полноценные сайты</li>
+          <li>Знания дизайна</li>
+          <li>Работа с пакетом Adobe</li>
+          <li>Составление документации на разработку/модернизации АИС</li>
+          <li>Составление Анализа Предмнетной области</li>
+          <li>Составление ТЗ, ТЭО, Технического Проекта</li>
+          <li>Составление диаграмм UML, IDEF, ARIS Express</li>
+          <li>Составление Руководства Пользователя, Администратора и Программиста</li>
+        </ul>
+        </div>
+    </div>
+    
   </article>
+  )
+}
+function Content3(){
+  return(
+    <div style={text_1}>Я разработчик веб-мультимедийных приложений, специализируюсь
+    на разработке веб-сайтов, дизайн-макетах сайтов, знаю backend и frontend разработку
+    .Имею 11 классов образования а также Среднее специальное образование. Отличаюсь усидчивостью,
+    умением масштабировать код, делать его гибким, а также вдумчиваостью. Умею находить общий язык с
+    коллективом, умею составлять проектную документацию (Анализ, ТЗ, ТЭО, ТП и т.д.). Умею строить
+    диаграммы IDEF, UML, ARIS. Берусь за задачи, с которыми смогу справиться</div>
+  )
+}
+function Contacts(){
+  let contacts = [
+    {img:phone, text:"+7 xxx xxx 45 48"},
+    {img:mail, text:"bael172@xmail.ru"},
+    {img:gmail, text:"bael171104@gmail.com"},
+    {img:whatsapp, text:"+7 xxx xxx 45 48"},
+    {img:telegram, text:"@ierihon"}
+  ]
+  let result = contacts.map((item)=><li style={{margin:"15px 0", listStyleType:"none"}}><img src={item.img} width="25px" margin-right="15px"></img> {item.text}</li>)
+  return (
+    <ul style={{padding:""}}>{result}</ul>
+  )
+}
+function Languages(){
+  let langs=[
+    {lang:"Английский",quantity:"3",quality:"B1 Средний"},
+    {lang:"Немецкий",quantity:"1", quality:"A1 Начальный"}
+  ]
+  const LanguageLevelIndicator = (indicator)=>{
+    const level = parseInt(indicator)
+    const circles = []
+    for(let i=0; i<6; i++){
+      circles.push(
+        <div key={i} style={{
+          width:"10px",
+          height:"10px",
+          borderRadius:"50%",
+          backgroundColor: i<level? '#1C7FBF' : '#DDDEDE',
+          margin:'0 5px 0'
+        }}></div>
+      )
+    }
+    return (
+      <div style={{display:"flex"}}>{circles}</div>
+    )
+  }
+  let result_1 = langs.map(function(item,index){
+    return <div id={index} style={{margin:"0 0 10px 0"}}>{item.lang}</div>
+
+    })
+  let result_2 = langs.map(function(item,index){
+    return (<div margin="0 0 10px 0">
+        {LanguageLevelIndicator(item.quantity)}
+        {item.quality}
+        </div>)
+  })
+  let collector = []
+  for(let i=0;i<langs.length;i++){
+    collector.push(<div>{result_1[i]}</div>)
+    collector.push(<div>{result_2[i]}</div>)
+  }
+  return(
+    <div className="container_languages" style={{margin:"0 0 20px 0"}}>
+      {collector}
+    </div>
+  )
+}
+function Skills(){
+  let skills = [
+    "Работа с Node.js",
+    "Работа с React",
+    "Дизайн сайтов в Figme",
+    "Photoshop",
+    "Illustator",
+    "Составление проектной документации",
+    "Составление ТЗ",
+    "Расчёт ТЭО",
+    "Основы Экономики"
+  ]
+  let result = skills.map(item => <li>{item}</li>)
+  return (
+    <>
+      {result}
+    </>
   )
 }
 
 function App() {
+  let half_width = window.innerWidth*0.5
   return (
     <div className="main">
-    <Photo></Photo>
-    <Header1></Header1>
-    <Content1></Content1>
-    <Header2></Header2>
-    <Content2></Content2>
+      <div style={{display:"inline"}}>
+        <Photo></Photo>
+        <Header_1 prop={{name:"Опыт работы"}}></Header_1>
+        <Content1></Content1>
+        <Header_1 prop={{name:"Образование", icon_src:mortarboard}}></Header_1>
+        <Content2></Content2>
+        <Header_1 prop={{name:"О себе", icon_src:account}}></Header_1>
+        <Content3></Content3>
+      </div>
+      <div style={{display:"inline"}}>
+        <Header_1 prop={{name:"Контакты", icon_src:contact_mail}}></Header_1>
+        <Contacts></Contacts>
+        <Header_1 prop={{name:"Языки", icon_src:translate}}></Header_1>
+        <Languages></Languages>
+        <Header_1 prop={{name:"Навыки", icon_src:pencil}}></Header_1>
+        <Skills></Skills>
+      </div>
     </div>
   );
 }
